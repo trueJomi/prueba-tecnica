5b868a93da73ae61a464f743dc253a8ca3e28006
Jose Miguel Vasquez <pixgel13040@gmail.com>
2025-05-23 16:09:45 -0500

    cambio nombre

--- backend/src/domain/api.domain.ts
+++ backend/src/domain/api.domain.ts
@@ -1,7 +1,7 @@
 import { type z } from 'zod'
 import { type schemaUsers } from '../validators/users.validate'
 import { getUser } from '../instructure/api.instructure'
-import { adapterToSImpleUser } from '../adapters/example.adapter'
+import { adapterToSImpleUser } from '../adapters/api.adapter'
 
 export async function domainGetUSer (data: z.infer<typeof schemaUsers>) {
   const users = await getUser(data.limit)
